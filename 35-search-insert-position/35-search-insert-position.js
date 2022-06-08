77028ee64f2caeb724a993ea9e1b1b3a1c2d92bc
정우용 <91973744+cleats01@users.noreply.github.com>
2022-06-08 20:28:58 +0900

    Time: 76 ms (56.14%), Space: 41.9 MB (90.13%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -5,8 +5,21 @@
  */
 var searchInsert = function(nums, target) {
     // 선형 검색
-    if (nums[nums.length-1] < target) return nums.length;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] >= target) return i;
+    // if (nums[nums.length-1] < target) return nums.length;
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (nums[i] >= target) return i;
+    // }
+    
+    /////////////////////////////////////////////////////////
+    
+    // 이진 검색 
+    let start = 0;
+    let end = nums.length;
+    while (start < end) {
+        let mid = Math.floor((start+end)/2);
+        if (target > nums[mid]) {
+            start = mid+1
+        } else end = mid
     }
+    return start
 };
