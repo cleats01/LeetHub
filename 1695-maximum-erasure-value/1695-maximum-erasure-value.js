58bc6fe9a5f0946d864ac19e6c9ea7129e0a5fb1
정우용 <91973744+cleats01@users.noreply.github.com>
2022-06-12 21:15:22 +0900

    Time: 6257 ms (10.00%), Space: 83.1 MB (8.33%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumUniqueSubarray = function(nums) {
+    // 연속된 중복 없는 합
+    let arr = [];
+    let max = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (!arr.includes(nums[i])) {
+            arr.push(nums[i]);
+            max = max < arr.reduce((a,c)=>a+c) ? arr.reduce((a,c)=>a+c) : max;
+        } else {
+            max = max < arr.reduce((a,c)=>a+c) ? arr.reduce((a,c)=>a+c) : max;
+            arr = arr.slice(arr.indexOf(nums[i])+1,i)
+            arr.push(nums[i]);
+        }
+        
+    }
+    return max;
+
+};
