03542dbcc90b8caf316110cdd1cefcb0eefe1aee
정우용 <91973744+cleats01@users.noreply.github.com>
2022-06-28 10:44:42 +0900

    Time: 71 ms (77.14%), Space: 42.4 MB (42.13%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.js
+++ 226-invert-binary-tree/226-invert-binary-tree.js
@@ -12,10 +12,8 @@
  */
 var invertTree = function(root) {
     if (!root) return root;
-    if (root) {
-        [root.left,root.right] = [root.right,root.left];
-        invertTree(root.left);
-        invertTree(root.right); 
-    };
+    [root.left,root.right] = [root.right,root.left];
+    invertTree(root.left);
+    invertTree(root.right); 
     return root;
 };
